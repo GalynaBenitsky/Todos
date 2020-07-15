cd5c9cbfcfb17fee54433859c6c6f7defe95034d
GalynaM <matsygingalyna@gmail.com>
2020-07-14 20:36:28 -0400

    Final Version

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,11 +2,17 @@ import React from 'react'
 
 export default function About() {
     return (
-        <React.Fragment>
-            <h1>About</h1>
-            <p>This is a ToDo List. It is a part of React Crush Course. Made by GM</p>
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    <React.Fragment>
+                        <h1>About</h1>
+                        <p>This is a ToDo List. It is a part of React Crush Course. Made by Galyna Matsygin.</p>
 
-        </React.Fragment>
+                    </React.Fragment>
+                </div>
+            </div>
+        </div>
     )
 }
 
